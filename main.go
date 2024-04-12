3a27ddd50b388ecf970a6132938833a19ba26959
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-04-12 19:07:43 +0530

    feat: Entire learning plan added

--- main.go
+++ main.go
@@ -30,5 +30,148 @@ func main(){
     // mapEx()
 
     // 10. Range
-    rangeEx()
+    // rangeEx()
+
+    // 11. Functions
+    // callFunc()
+
+    // 12. Variadic functions
+    // variadic()
+
+    // 13. Closures
+    closure() 
+
+    // 14. Recursion
+
+
+    // 15. Pointers
+
+    // 16. Strings and Runes
+
+    // 17. Structs
+
+    // 18. Methods
+
+    // 19. Interfaces
+
+    // 20. Struct Embedding
+
+    // 21. Generics
+
+    // 22. Errors
+
+    // 23. Custom Errors
+
+    // 24. Goroutines
+
+    // 25. Channels
+
+    // 26. Channel Buffering
+
+    // 27. Channel Synchronization
+
+    // 28. Channel Directions
+
+    // 29. Select
+
+    // 30. Timeouts
+
+    // 31. Non-Blocking Channel Operations
+    
+    // 32. Closing Channels
+
+    // 33. Timers
+
+    // 34. Tickers
+    
+    // 35. Worker Pools
+
+    // 36. WaitGroups
+
+    // 37. Rate Limiting (api stuff ?)
+    
+    // 38. Atomic Counters
+
+    // 39. Mutexes
+
+    // 40. Stateful Goroutines
+
+    // 41. Sorting
+    
+    // 42. Sorting by Functions
+
+    // 43. Panic
+
+    // 44. Defer
+
+    // 45. Recover
+
+    // 46. String Functions
+
+    // 47. String Formatting
+
+    // 48. Text Templates
+   
+    // 49. Regular expressions
+
+    // 50. JSON
+
+    // 51. XML
+
+    // 52. Time 
+
+    // 53. Epoch
+
+    // 54. Time Formatting / Parsing
+   
+    // 55. Random Numbers
+
+    // 56. Number Parsing
+
+    // 59. URL Parsing
+
+    // 60. SHA256 Hashes
+
+    // 61. Base64 Encoding
+
+    // 62. Reading files
+
+    // 63. Writing files
+
+    // 64. Line Filters
+
+    // 65. File Paths
+
+    // 66. Directories
+
+    // 69. Temporary Files and Directories
+
+    // 70. Embed Directive
+
+    // 71. Testing and Benchmarking
+
+    // 72. Command-line arguments
+
+    // 72. Command-line flags
+
+    // 73. Command-line subcommands
+
+    // 74. Environement Variables
+
+    // 75. Logging
+
+    // 76. HTTP Client
+
+    // 77. HTTP Server
+
+    // 78. Context
+
+    // 79. Spawning Processes
+
+    // 80. Exec'ing Processes
+
+    // 81. Signals
+
+    // 82. Exit
+
 }
